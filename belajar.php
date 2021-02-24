613c5b44d93947665e78829d482e52a695f110e6
rizkimuy <rizkimuhammad127@gmail.com>
2021-02-24 15:16:33 +0700

    belajar php dasar

---
+++ belajar.php
@@ -0,0 +1,16 @@
+<?php
+
+    define("PHI",3.14);
+    $jari_jari = 7;
+    $luas_lingkaran = 3.14 * $jari_jari * $jari_jari;
+    echo $jari_jari;
+
+    $angka = 1;
+    $nama = "rizki";
+    $angkadesimal = 3.5;
+    echo $angka;
+    echo "</br>";
+    echo $nama;
+
+
+?>
